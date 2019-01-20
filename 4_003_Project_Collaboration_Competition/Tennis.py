201d2d303a500d1bbaeb99a04312a340033d69aa
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-01-20 04:11:21 -0800

    feat: Adapt DDGP - Algorithm
    Adapt DDPG Algorithm to the current environment, as expected, the agent
    was not able to learn, as this is a competitive task. Not as the
    "multi-arm" case, where all agent, were learning towards a common goal.
    
    Next step: MADDPG - Implementation

--- 4_003_Project_Collaboration_Competition/Tennis.py
+++ 4_003_Project_Collaboration_Competition/Tennis.py
@@ -4,7 +4,70 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 from unityagents import UnityEnvironment
-# from ddpg_agent_multiarm import Agent
+from ddpg_agent_tennis import Agent
+
+
+def ddpg(agent, n_episodes=2000, max_t=800, num_agents=2, consec_learn_iter=5):
+    """ DDPG - Algorithm implementation"""
+    scores_episodes = []
+    scores_episodes_deque = deque(maxlen=100)
+
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=True)[brain_name]
+        states = env_info.vector_observations
+        scores = np.zeros(num_agents)
+
+        for t in range(max_t):
+            # Agent decision and interaction
+            actions = []
+
+            for state in states:
+                agent.reset()
+                actions.append(agent.act(state))
+
+            env_info = env.step(actions)[brain_name]
+
+            # Feedback on action
+            next_states = env_info.vector_observations
+            rewards = env_info.rewards
+            dones = env_info.local_done
+
+            # Experience saving
+            for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
+                agent.save_experience(state, action, reward, next_state, done)
+            agent.update_counter()
+
+            # Update values
+            if agent.train:
+                for _ in range(consec_learn_iter):
+                    agent.trigger_learn()
+
+            states = next_states
+            scores += rewards
+
+            if np.any(dones):
+                break
+
+        # Scoring & Terminal information
+        scores_episodes.append(np.mean(scores))
+        scores_episodes_deque.append(np.mean(scores))
+
+        print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, np.mean(scores_episodes_deque),
+                                                                          np.mean(scores)), end="")
+
+        if i_episode % 100 == 0:
+            torch.save(agent.actor_local.state_dict(), 'checkpoint_actor_20_arms.pth')
+            torch.save(agent.critic_local.state_dict(), 'checkpoint_critic_20_arms.pth')
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_episodes_deque)))
+
+        if np.mean(scores_episodes_deque) > 30.0:
+            torch.save(agent.actor_local.state_dict(), 'model_actor_20_arms.pth')
+            torch.save(agent.critic_local.state_dict(), 'model_critic_20_arms.pth')
+            print('\rEpisode employed for completing the challenge {}'.format(i_episode))
+
+            break
+
+    return scores_episodes
 
 
 #%% Load Tennis environment
@@ -24,8 +87,19 @@ num_agents = len(env_info.agents)
 
 #%% DDPG - Agent Training
 
+# Initialize Agent
+agent = Agent(state_size=state_size, action_size=action_size, random_seed=11)
 
+# Execute DDPG - Learning
+score = ddpg(agent)
 
+# Plot results
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(1, len(score)+1), score)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
 
 #%% Actions for adapting code
 # TODO: use and delete, TEMPORAL
---
+++ 4_003_Project_Collaboration_Competition/model.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Actor(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc_units_1=32, fc_units_2=32):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(Actor, self).__init__()
+        self.seed = torch.manual_seed(seed)
+
+        self.fc1 = nn.Linear(state_size, fc_units_1)
+        self.ln_1 = nn.LayerNorm(fc_units_1)
+        self.fc2 = nn.Linear(fc_units_1, fc_units_2)
+        self.ln_2 = nn.LayerNorm(fc_units_2)
+        self.fc3 = nn.Linear(fc_units_2, action_size)
+        self.ln_3 = nn.LayerNorm(action_size)
+
+    def forward(self, state):
+        """Build an actor (policy) network that maps states -> actions."""
+        x = self.fc1(state)
+        x = self.ln_1(F.relu(x))
+        x = self.fc2(x)
+        x = self.ln_2(F.relu(x))
+        x = self.fc3(x)
+        x = self.ln_3(F.relu(x))
+
+        return F.tanh(x)
+
+
+class Critic(nn.Module):
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size, seed, fcs1_units=256, fc_units_2=256, fc_units_3=128, fc_units_4=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fcs1_units (int): Number of nodes in the first hidden layer
+            fc2_units (int): Number of nodes in the second hidden layer
+        """
+        super(Critic, self).__init__()
+        self.seed = torch.manual_seed(seed)
+
+        self.fcs1 = nn.Linear(state_size, fcs1_units)
+        self.ln_1 = nn.LayerNorm(fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units+action_size, fc_units_2)
+        self.ln_2 = nn.LayerNorm(fc_units_2)
+        self.fc3 = nn.Linear(fc_units_2, fc_units_3)
+        self.ln_3 = nn.LayerNorm(fc_units_3)
+        self.fc4 = nn.Linear(fc_units_3, fc_units_4)
+        self.ln_4 = nn.LayerNorm(fc_units_4)
+        self.fc5 = nn.Linear(fc_units_4, 1)
+
+    def forward(self, state, action):
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        xs = self.fcs1(state)
+        xs = self.ln_1(F.relu(xs))
+        x = torch.cat((xs, action), dim=1)
+        x = self.fc2(x)
+        x = self.ln_2(F.relu(x))
+        x = self.fc3(x)
+        x = self.ln_3(F.relu(x))
+        x = self.fc4(x)
+        x = self.ln_4(F.relu(x))
+        return self.fc5(x)
