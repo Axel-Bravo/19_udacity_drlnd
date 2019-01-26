3a8bb4e78dfa3b5139d646af0ed9e02062554ab5
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-01-26 11:10:16 -0800

    feat: Reduce critic
    - Reduction un critic size
    - Change of "relu" for "leaky_relu"

--- 4_003_Project_Collaboration_Competition/ddpg.py
+++ 4_003_Project_Collaboration_Competition/ddpg.py
@@ -8,13 +8,13 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e6)  # replay buffer size
-BATCH_SIZE = 256        # minibatch size
-GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1.5e-4       # learning rate of the actor
-LR_CRITIC = 3e-4      # learning rate of the critic
-WEIGHT_DECAY = 0.0001   # L2 weight decay
+BUFFER_SIZE = int(1e6)    # replay buffer size
+BATCH_SIZE = 64          # minibatch size
+GAMMA = 0.99              # discount factor
+TAU = 1e-3                # for soft update of target parameters
+LR_ACTOR = 1e-4           # learning rate of the actor
+LR_CRITIC = 2e-4          # learning rate of the critic
+WEIGHT_DECAY = 0.0001     # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -58,8 +58,8 @@ class Agent(object):
 
     def memorize(self, state, action, reward, next_tate, done):
         """Inscribes in the memory the experiences"""
-        for agent in range(self.num_agents):
-            self.memory.add(state[agent, :], action[agent, :], reward[agent], next_tate[agent, :], done[agent])
+
+        self.memory.add(state, action, reward, next_tate, done)
 
     def act(self, states, add_noise=True):
         """Returns actions for given state as per current policy."""
@@ -96,32 +96,40 @@ class Agent(object):
         if len(self.memory) > BATCH_SIZE:
             self.exploration *= 0.999
 
-            experiences = self.memory.sample()
-            states, actions, rewards, next_states, dones = experiences
+            experience = self.memory.sample()
+            states, actions, rewards, next_states, dones = experience
 
-            # ---------------------------- update critic ---------------------------- #
-            # Get predicted next-state actions and Q values from target models
-            actions_next = self.actor_target(next_states)
-            Q_targets_next = self.critic_target(next_states, actions_next)
-            # Compute Q targets for current states (y_i)
-            Q_targets = rewards + (GAMMA * Q_targets_next * (1 - dones))
-            # Compute critic loss
-            Q_expected = self.critic_local(states, actions)
-            critic_loss = F.mse_loss(Q_expected, Q_targets)
-            # Minimize the loss
-            self.critic_optimizer.zero_grad()
-            critic_loss.backward()
-            # torch.nn.utils.clip_grad_norm(self.critic_local.parameters(), 1)  # Gradient clipping
-            self.critic_optimizer.step()
+            for agent in range(self.num_agents):
+                state = states[(agent*BATCH_SIZE):((agent+1)*BATCH_SIZE)]
+                action = actions[(agent*BATCH_SIZE):((agent+1)*BATCH_SIZE)]
+                reward = rewards[:, agent]
+                next_state = next_states[(agent*BATCH_SIZE):((agent+1)*BATCH_SIZE)]
+                done = dones[:, agent]
 
-            # ---------------------------- update actor ---------------------------- #
-            # Compute actor loss
-            actions_pred = self.actor_local(states)
-            actor_loss = -self.critic_local(states, actions_pred).mean()
-            # Minimize the loss
-            self.actor_optimizer.zero_grad()
-            actor_loss.backward()
-            self.actor_optimizer.step()
+                # ---------------------------- update critic ---------------------------- #
+                # Get predicted next-state actions and Q values from target models
+                actions_next = self.actor_target(next_state)
+                Q_targets_next = self.critic_target(next_state, actions_next)
+                # Compute Q targets for current states (y_i)
+                Q_targets = reward + (GAMMA * Q_targets_next * (1 - done))
+                # Compute critic loss
+                Q_expected = self.critic_local(state, action)
+                critic_loss = F.mse_loss(Q_expected, Q_targets)
+                # Minimize the loss
+                self.critic_optimizer.zero_grad()
+                critic_loss.backward()
+                torch.nn.utils.clip_grad_norm(self.critic_local.parameters(), 1)  # Gradient clipping
+                self.critic_optimizer.step()
+
+                # ---------------------------- update actor ---------------------------- #
+                # Compute actor loss
+                actions_pred = self.actor_local(state)
+                actor_loss = -self.critic_local(state, actions_pred).mean()
+                # Minimize the loss
+                self.actor_optimizer.zero_grad()
+                actor_loss.backward()
+                torch.nn.utils.clip_grad_norm(self.critic_local.parameters(), 1)  # Gradient clipping
+                self.actor_optimizer.step()
 
             # ----------------------- update target networks ----------------------- #
             self.soft_update(self.critic_local, self.critic_target, TAU)
--- 4_003_Project_Collaboration_Competition/model.py
+++ 4_003_Project_Collaboration_Competition/model.py
@@ -25,9 +25,9 @@ class Actor(nn.Module):
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        x = F.relu6(self.fc1(state))
-        x = F.relu6(self.fc2(x))
-        x = F.relu6(self.fc3(x))
+        x = F.leaky_relu(self.fc1(state))
+        x = F.leaky_relu(self.fc2(x))
+        x = F.leaky_relu(self.fc3(x))
 
         return F.tanh(x)
 
@@ -35,7 +35,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=256, fc_units_2=128, fc_units_3=64, fc_units_4=32):
+    def __init__(self, state_size, action_size, seed, fcs1_units=256, fc_units_2=128, fc_units_3=64):
         """Initialize parameters and build model.
         Params
         ======
@@ -51,8 +51,7 @@ class Critic(nn.Module):
         self.fcs1 = nn.Linear(state_size, fcs1_units)
         self.fc2 = nn.Linear(fcs1_units+action_size, fc_units_2)
         self.fc3 = nn.Linear(fc_units_2, fc_units_3)
-        self.fc4 = nn.Linear(fc_units_3, fc_units_4)
-        self.fc5 = nn.Linear(fc_units_4, 1)
+        self.fc4 = nn.Linear(fc_units_3, 1)
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
@@ -60,5 +59,5 @@ class Critic(nn.Module):
         x = torch.cat((xs, action), dim=1)
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
-        return self.fc5(x)
+
+        return self.fc4(x)
