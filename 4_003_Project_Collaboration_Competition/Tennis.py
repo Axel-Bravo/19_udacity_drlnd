6efa10f992d2558dbf574a1733a84c23ad081a4b
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-01-26 04:59:58 -0800

    feat: Start Branch

--- 4_003_Project_Collaboration_Competition/Tennis.py
+++ 4_003_Project_Collaboration_Competition/Tennis.py
@@ -102,24 +102,5 @@ plt.ylabel('Score')
 plt.xlabel('Episode #')
 plt.show()
 
-#%% Actions for adapting code
-# TODO: use and delete, TEMPORAL
-for i in range(1, 6):                                      # play game for 5 episodes
-    env_info = env.reset(train_mode=False)[brain_name]     # reset the environment    
-    states = env_info.vector_observations                  # get the current state (for each agent)
-    scores = np.zeros(num_agents)                          # initialize the score (for each agent)
-    while True:
-        actions = np.random.randn(num_agents, action_size) # select an action (for each agent)
-        actions = np.clip(actions, -1, 1)                  # all actions between -1 and 1
-        env_info = env.step(actions)[brain_name]           # send all actions to tne environment
-        next_states = env_info.vector_observations         # get next state (for each agent)
-        rewards = env_info.rewards                         # get reward (for each agent)
-        dones = env_info.local_done                        # see if episode finished
-        scores += env_info.rewards                         # update the score (for each agent)
-        states = next_states                               # roll over states to next time step
-        if np.any(dones):                                  # exit loop if episode finished
-            break
-    print('Score (max over agents) from episode {}: {}'.format(i, np.max(scores)))
-
 #%% Environement- Close
 env.close()
