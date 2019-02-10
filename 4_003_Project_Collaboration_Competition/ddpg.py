18d110e71590dede7e5883fe86070291354b5cf6
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-02-10 01:18:22 -0800

    feat: add "full_state" into "ReplayBuffer"
    In order to acccomodate the new required "full_state" information for
    the critic, in the maddpg, we need to save into the replay buffer also
    the full states

--- 4_003_Project_Collaboration_Competition/ddpg.py
+++ 4_003_Project_Collaboration_Competition/ddpg.py
@@ -74,7 +74,7 @@ class DDGP(object):
         self.noise = OUNoise(action_size, random_seed)
 
         # Replay memory
-        self.memory = ReplayBuffer(self.buffer_size, random_seed)
+        self.memory = ReplayBuffer(self.buffer_size, self.batch_size, random_seed)
 
         # Step memory
         self.step_mem = 0
@@ -196,6 +196,49 @@ class DDGP(object):
         torch.save(save_dict, os.path.join(save_path, self.name + 'i_episode-{}.pt'.format(iteration)))
 
 
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, buffer_size, batch_size, seed):
+        """Initialize a ReplayBuffer object.
+        Params
+        ======
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+        """
+        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "full_state", "action", "reward",
+                                                                "next_state", "next_full_state", "done"])
+        self.seed = random.seed(seed)
+
+    def add(self, state, full_state, action, reward, next_state, next_full_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, full_state, action, reward, next_state, next_full_state, done)
+        self.memory.append(e)
+
+    def sample(self):
+        """Randomly sample a batch of experiences from memory."""
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        full_state = torch.from_numpy(np.vstack([e.full_state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(
+            device)
+        next_full_state = torch.from_numpy(np.vstack([e.next_full_state for e in experiences if e is not None])).\
+            float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(
+            device)
+
+        return states, full_state, actions, rewards, next_states, next_full_state, dones
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
+
+
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
 
@@ -219,43 +262,3 @@ class OUNoise:
         return self.state
 
 
-class ReplayBuffer:
-    """Fixed-size buffer to store experience tuples."""
-
-    def __init__(self, buffer_size, seed):
-        """Initialize a ReplayBuffer object.
-        Params
-        ======
-            buffer_size (int): maximum size of buffer
-            batch_size (int): size of each training batch
-        """
-        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
-        self.seed = random.seed(seed)
-
-    def add(self, state, action, reward, next_state, done):
-        """Add a new experience to memory."""
-        e = self.experience(state, action, reward, next_state, done)
-        self.memory.append(e)
-
-    def sample(self):
-        """Randomly sample a batch of experiences from memory."""
-        experiences = random.sample(self.memory, k=self.batch_size)
-
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(
-            device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(
-            device)
-
-        return states, actions, rewards, next_states, dones
-
-    def __len__(self):
-        """Return the current size of internal memory."""
-        return len(self.memory)
-
-
-def proces_samples(input_list):
-    return torch.from_numpy(np.array(input_list))
